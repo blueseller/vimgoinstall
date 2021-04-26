70f3492252b0eb25df1620ff1336c08b3b16f1d2
liukai <liukai198612@gmail.com>
2021-04-26 10:42:41 +0800

    add vim plugin install

--- install_vim.sh
+++ install_vim.sh
@@ -21,3 +21,21 @@ tar zxvf vim.tar.gz
 
 sudo make VIMRUNTIMEDIR=/usr/share/vim/vim82
 make install
+
+# 安装vim 插件管理工具  bundle
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
+sudo apt install python3-pip
+
+pip3 install pynvim
+
+# 安装字体
+
+git clone https://github.com/ryanoasis/nerd-fonts.git --depth=1
+
+./install.sh DroidSansMono
+￼
+./install.sh Hack
+
+# nerd tree 乱码解决方式
+需要使用字符集 nerd sans mono 就可以了
